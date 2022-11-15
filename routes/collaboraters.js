22dc25feb8a5031e9886d52c5f06e301b4c56ab1
firasyazid <98762038+firasyazid@users.noreply.github.com>
2022-11-15 12:49:09 +0100

    push

--- routes/collaboraters.js
+++ routes/collaboraters.js
@@ -2,6 +2,34 @@ const {Collaborater} = require('../models/collaborater');
 const express = require('express');
 const router = express.Router();
 const { Category } = require('../models/categories');
+const multer = require('multer');
+ 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if (isValid) {
+            uploadError = null;
+        }
+      cb(null, 'public/uploads')
+    },
+
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.split(' ').join('-');
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`);
+    }
+  })
+
+  const uploadOptions = multer({ storage: storage });
+
 
  
 
@@ -25,17 +53,23 @@ router.get('/:id', async(req,res)=>{
 }) 
 
 
-router.post('/', async (req,res)=>{
+router.post('/',uploadOptions.single('image'), async (req,res)=>{
     
     const category = await Category.findById(req.body.category);
     if (!category) return res.status(400).send('Invalid Category');
+    const file = req.file;
+    if (!file) return res.status(400).send('No image in the request');
+
+
+    const fileName = req.file.filename
+   const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
 
     let collaborater = new Collaborater({
         name: req.body.name,
         lastname: req.body.lastname,
         location: req.body.location,
         phone: req.body.phone,
-        image: req.body.image,
+        image:`${basePath}${fileName}` ,
         category: req.body.category,
     })
     collaborater = await collaborater.save();
